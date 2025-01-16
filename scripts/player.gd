0cac32dc4e6e674685c7b5649aa3aee4e43dfc10
Some1new00 <some1new00@gmail.com>
2025-01-15 23:40:27 -0500

    Update
    Cam SMOOthing

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,25 +1,35 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
+@onready var player_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var can_control : bool = true
+const SPEED = 500.0
 const JUMP_VELOCITY = -400.0
+func turn():
+	if velocity.x < 0:
+		player_sprite.flip_h = true
+	if velocity.x > 0:
+		player_sprite.flip_h = false
 
-
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
+func _physics_process(delta) -> void:
+	print (Engine.get_frames_per_second())
+	
+	if not can_control: return
+	velocity += get_gravity() * delta
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		
+		velocity.y += JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("Left", "Right")
+	var direction :float= Input.get_axis("Left", "Right")
 	if direction:
+		$AnimatedSprite2D.play("Run")
 		velocity.x = direction * SPEED
+	elif velocity.y < 0:
+		$AnimatedSprite2D.play("Jump")
+	elif not is_on_floor():
+		$AnimatedSprite2D.play("Fall")
 	else:
+		$AnimatedSprite2D.play("Idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	turn()
 
 	move_and_slide()
